1835fd0899ce46a69c27601429a64f40abe85a7f
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-06-16 02:42:39 +0300

    Building the Quiz App UI

--- lib/main.dart
+++ lib/main.dart
@@ -10,11 +10,65 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Quiz App',
-      theme: ThemeData(
-        useMaterial3: true
+      theme: ThemeData(useMaterial3: false),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Quiz App'),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              const Padding(
+                padding: EdgeInsets.only(top: 16.0, bottom: 16.0),
+                child: Text(
+                  'What is your favorite sports?',
+                  style: TextStyle(
+                    fontSize: 26.0,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.blue,
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Football'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Basketball'),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: SizedBox(
+                  width: double.infinity,
+                  height: 60.0,
+                  child: ElevatedButton(
+                    onPressed: () {},
+                    child: const Text('Volleyball'),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
 }
-
